f91df0296bb9ce59d4eac9189d1418d762310501
Marcos González Penín <a20marcosgp@ATEIS.LOCAL>
2022-02-03 20:06:50 +0100

    Navegación completa

--- app/src/main/java/com/example/navegacionfragmentsandroid/MainActivity.kt
+++ app/src/main/java/com/example/navegacionfragmentsandroid/MainActivity.kt
@@ -2,13 +2,18 @@ package com.example.navegacionfragmentsandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.navegacionfragmentsandroid.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +21,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.appBarMain.toolbar)
 
+        appBarConfiguration = AppBarConfiguration(
+            setOf(  // Los que pasamos son los que se consideran top-level (no se muestra el icono "atrás"
+                R.id.nav_main,
+            ), binding.drawerLayout
+        )
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
